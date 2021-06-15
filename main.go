6ac8dc033e59c5be28579a8fb15f2a94982e9fba
apristupa <a.pristupa@facecast.net>
2021-06-15 19:25:31 +0300

    fix bag

--- main.go
+++ main.go
@@ -21,28 +21,15 @@ type Rtmp struct {
 const ffmpegGetRtmp = "-i {{.}} -vcodec libx264 -max_muxing_queue_size 1024 -movflags empty_moov+omit_tfhd_offset+frag_keyframe+default_base_moof -max_interleave_delta 0 -frag_duration 1 -b:v 1024k -c:a aac -f mp4 pipe:1"
 const mp4boxHLS = "-i stdin -o file.m3u8"
 
-func (self *Rtmp) runMP4Box(f *exec.Cmd, r *io.PipeReader) {
-	cmd := exec.Command("gpac", strings.Split(mp4boxHLS, " ")...)
-	cmd.Stdin = r
+func (self *Rtmp) runMP4Box(cmd *exec.Cmd) {
 
-	check := 0
+	err := cmd.Start()
 
-	for {
-		log.Print(cmd.ProcessState)
-		if check != 1 {
-			err := cmd.Start()
-
-			log.Print(cmd.String())
-			if err != nil {
-				check = 1
-				log.Err(err).Msg("gpac error")
-			} else {
-				check = 1
-				log.Print("vse good gpac")
-			}
-			self.Close = true
-		}
+	if err != nil {
+		log.Err(err).Msg("gpac error")
 	}
+	cmd.Wait()
+	self.Close = true
 }
 
 func main() {
@@ -51,10 +38,12 @@ func main() {
 	baseUrl := os.Args[1]
 
 	r, w := io.Pipe()
+
 	rtmpHave, err := regexp.MatchString(`^rtmp*`, baseUrl)
 	if err != nil {
 		log.Print(err)
 	}
+
 	tmp, err := template.New("rtmp").Parse(ffmpegGetRtmp)
 	if err != nil {
 		log.Error().Err(err).Msg("template.New")
@@ -81,24 +70,22 @@ func main() {
 			}
 			cmd := exec.Command("ffmpeg", strings.Split(com.String(), " ")...)
 			cmd.Stdout = w
+			cmdmp := exec.Command("gpac", strings.Split(mp4boxHLS, " ")...)
+			cmdmp.Stdin = r
 
-			go stream.runMP4Box(cmd, r)
+			go stream.runMP4Box(cmdmp)
+
 			cmd.Stderr = os.Stdout
-			log.Print(cmd.String())
-
 			err := cmd.Start()
+
 			if err != nil {
 				stream.Open = false
 				log.Err(err).Msg("ffmpeg error")
 			} else {
 				stream.Open = true
-				log.Print("vse good")
 			}
 			for !stream.Close {
-
 			}
-
 		}
 	}
-
 }
